d53352e2f6367e81487fac93207e6c0ae3d52522
Unawatuna Hewage Chanupa Chinthal <chanupa@codelk.com>
2026-02-06 03:31:32 +0530

    fix: resolve mounting and secret synchronization issues

--- src/backend/lib/s3-utils.ts
+++ src/backend/lib/s3-utils.ts
@@ -1,17 +1,19 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const region = process.env.AWS_REGION || "ap-southeast-2";
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID!;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY!;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const primaryBucketName = process.env.AWS_S3_BUCKET_NAME!;
 const backupBucketName = process.env.AWS_S3_BACKUP_BUCKET_NAME!;
 
 const s3Client = new S3Client({
     region,
-    credentials: {
-        accessKeyId,
-        secretAccessKey,
-    },
+    ...(accessKeyId && secretAccessKey ? {
+        credentials: {
+            accessKeyId,
+            secretAccessKey,
+        }
+    } : {}),
 });
 
 /**
