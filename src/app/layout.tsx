684f454661f71ed9319df50ca0fe5274da060b21
Unawatuna Hewage Chanupa Chinthal <chanupa@codelk.com>
2026-02-15 03:05:15 +0530

    fix(layout): force viewport meta tag in head to resolve mobile scaling issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <ToastProvider>
